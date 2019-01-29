f5528970491ea3608187182e137698e3ddaafa52
Frank Purcell <fxpurcell@gmail.com>
2019-01-29 10:52:19 -0800

    initial add of classes

---
+++ ott/sumdb/model/base.py
@@ -0,0 +1,103 @@
+import abc
+import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, DateTime
+
+import logging
+log = logging.getLogger(__file__)
+
+
+class _Base(object):
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    created = Column(DateTime, default=datetime.datetime.now())
+    updated = Column(DateTime, default=datetime.datetime.now())
+
+    lang = "en"
+
+    @classmethod
+    def clear_tables(cls, session):
+        log.warning("called from parent, so no idea what tables to clear")
+
+    @classmethod
+    def make_mapper(cls, tablename, poly_column=None):
+        """ todo """
+        ret_val = {
+            'polymorphic_identity': tablename,
+            'with_polymorphic': '*'
+        }
+        if poly_column:
+            ret_val['polymorphic_on'] = poly_column
+        return ret_val
+
+    @classmethod
+    def from_dict(cls, attrs):
+        clean_dict = cls.make_record(attrs)
+        c = cls(**clean_dict)
+        return c
+
+    def to_dict(self):
+        """
+        convert a SQLAlchemy object into a dict that is serializable to JSON
+        """ 
+        ret_val = self.__dict__.copy()
+
+        # the __dict__ on a SQLAlchemy object contains hidden crap that we delete from the class dict
+        # (not crazy about this hack, but ...) 
+        if set(['_sa_instance_state']).issubset(ret_val):
+            del ret_val['_sa_instance_state']
+
+        # convert time, date & datetime, etc... objects to iso formats
+        for k in ret_val.keys():
+            v = ret_val[k] 
+            if hasattr(v,"isoformat"):
+                ret_val[k] = v.isoformat()
+
+        return ret_val
+
+    @classmethod
+    def to_dict_list(cls, list):
+        """
+        apply to_dict() to all elements in list, and return new / resulting list...
+        """
+        ret_val = []
+        for l in list:
+            if hasattr(l,"to_dict"):
+                l = l.to_dict()
+            ret_val.append(l)
+        return ret_val
+
+    @classmethod
+    def bulk_load(cls, engine, records, remove_old=True):
+        """
+        load a bunch of records at once from a list (first clearing out the table).
+        note that the records array has to be dict structures, ala
+        http://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.Connection.execute
+        """
+        table = cls.__table__
+        if remove_old:
+            engine.execute(table.delete())
+        engine.execute(table.insert(), records)
+
+    @classmethod
+    def set_schema(cls, schema):
+        # if this is a database table, set the schema
+        if hasattr(cls, '__table__'):
+            cls.__table__.schema = schema
+
+        # bit of recursion to hit sub classes
+        for c in cls.__subclasses__():
+            c.set_schema(schema)
+
+    @classmethod
+    def set_geometry(cls, is_geospatial=False):
+        if is_geospatial:
+            if hasattr(cls, 'add_geometry_column'):
+                cls.add_geometry_column()
+
+            # bit of recursion to hit sub classes
+            for c in cls.__subclasses__():
+                c.set_geometry(is_geospatial)
+
+
+Base = declarative_base(cls=_Base)
---
+++ ott/sumdb/model/vehicle.py
@@ -0,0 +1,146 @@
+import datetime
+from sqlalchemy import Column, Index, Integer, Numeric, String, DateTime
+from sqlalchemy.sql import func, and_
+from sqlalchemy.orm import deferred, object_session, relationship
+
+from .base import Base
+from .vehicle_position import VehiclePosition
+
+import logging
+log = logging.getLogger(__file__)
+
+
+class Vehicle(Base):
+    __tablename__ = 'rt_vehicles'
+
+    vehicle_id = Column(String, nullable=False)
+    license_plate = Column(String)
+
+    positions = relationship(
+        'VehiclePosition',
+        primaryjoin='Vehicle.vehicle_id == VehiclePosition.vehicle_id',
+        foreign_keys='(Vehicle.vehicle_id)',
+        uselist=True, viewonly=True,
+        backref="vehicle"
+    )
+
+    def __init__(self, agency, vehicle_id, license_plate=None):
+        self.agency = agency
+        self.vehicle_id = vehicle_id
+        self.license_plate = license_plate
+
+    @classmethod
+    def clear_tables(cls, session, agency):
+        """
+        clear out the positions and vehicles tables
+        """
+        VehiclePosition.clear_tables(session, agency)
+        session.query(Vehicle).filter(Vehicle.agency == agency).delete()
+        session.commit()
+
+    @classmethod
+    def parse_gtfsrt_feed(cls, session, agency, feed):
+        if feed and feed.entity and len(feed.entity) > 0:
+            VehiclePosition.clear_latest_column(session, agency)
+            super(Vehicle, cls).parse_gtfsrt_feed(session, agency, feed)
+
+    @classmethod
+    def parse_gtfsrt_record(cls, session, agency, record, timestamp):
+        """ create or update new Vehicles and positions
+            :return Vehicle object
+        """
+        ret_val = None
+        v = None
+
+        # step 1: query db for vehicle
+        try:
+            # step 1a: get inner 'vehicle' record
+            record = record.vehicle
+
+            # step 1b: see if this is an existing vehicle
+            q = session.query(Vehicle).filter(
+                and_(
+                    Vehicle.vehicle_id == record.vehicle.id,
+                    Vehicle.agency == agency,
+                )
+            )
+            v = q.first()
+        except Exception as err:
+            log.exception(err)
+
+        try:
+            # step 2: we didn't find an existing vehicle in the Vehicle table, so add a new one
+            if v is None:
+                v = Vehicle(agency, record.vehicle.id, record.vehicle.license_plate)
+                session.add(v)
+                session.commit()
+                session.flush()
+
+            # step 2b: set ret_val to our Vehicle (old or new)
+            ret_val = v
+
+            # step 3: update the position record if need be
+            v.update_position(session, agency, record)
+        except Exception as err:
+            log.exception(err)
+            session.rollback()
+        finally:
+            try:
+                session.commit()
+                session.flush()
+            except Exception as err:
+                log.exception(err)
+                session.rollback()
+
+        return ret_val
+
+    def update_position(self, session, agency, data, time_span=144):
+        """ query the db for a position for this vehicle ... if the vehicle appears to be parked in the
+            same place as an earlier update, update the 
+            NOTE: the position add/update needs to be committed to the db by the caller of this method 
+        """
+
+        # step 0: cast some variables
+        lat = round(data.position.latitude,  6)
+        lon = round(data.position.longitude, 6)
+
+        # step 1: get position object from db ...criteria is to find last position 
+        #          update within an hour, and the car hasn't moved lat,lon
+        hours_ago = datetime.datetime.now() - datetime.timedelta(hours=time_span)
+        p = None
+        try:
+            q = session.query(VehiclePosition).filter(
+                and_(
+                    VehiclePosition.vehicle_fk == self.id,
+                    VehiclePosition.updated >= hours_ago,
+                    VehiclePosition.lat == lat,
+                    VehiclePosition.lon == lon,
+                )
+            )
+            p = q.first()
+        except Exception as err:
+            log.exception(err)
+
+        # step 2: we didn't find an existing position in the Position history table, so add a new one
+        try:
+            if p is None:
+                p = VehiclePosition()
+                p.vehicle_fk = self.id
+                p.agency = agency
+                p.set_attributes(data)
+                p.set_position(lat, lon)
+                session.add(p)
+            else:
+                p.set_updated()
+
+        except Exception as err:
+            log.exception(err)
+            session.rollback()
+        finally:
+            try:
+                session.commit()
+                session.flush()
+            except Exception as err:
+                log.exception(err)
+                session.rollback()
+        return p
